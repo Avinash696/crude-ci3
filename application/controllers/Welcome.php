38d33fccad94aac44c6b6fbece384de06e72c187
Avinash696 <arawat696@gmail.com>
2022-08-02 03:30:55 +0530

    ci3 crude

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,44 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	public function __construct()
+	{
+	parent::__construct();
+
+	$this->load->model('Emp');
+	}
+
+	public function index()
+	{
+    $data['rows'] = $this->Emp->getAll();
+
+    $this->load->view('home', $data);
+	}
+	
+	public function save(){
+        $data=[
+            'name'=>$this->input->post('name'),
+            'password'=>$this->input->post('password')
+        ];
+
+        $this->Emp->addrecord($data);
+    }
+	public function delete()
+	{	
+		$id=$this->input->post('id');
+		$item = $this->Emp->deleteRecord($id);
+			echo 'Deleted';
+	}
+	public function update()
+	{	$id=$this->input->post('id');
+		$username=$this->input->post('name');
+		$password=$this->input->post('password');
+
+		
+        $this->Emp->updateRecord($username,$password,$id);
+	
+			echo 'Updated';
+	}
+}
---
+++ application/models/Emp.php
@@ -0,0 +1,40 @@
+<?php 
+
+class Emp extends CI_Model {
+    public function __construct (){
+        parent::__construct ();
+  
+     }
+
+    function getAll(){
+
+        $q  = $this->db->query("SELECT * from ci4table");
+
+        if($q->num_rows() > 0) {
+
+            foreach ($q->result() as $row) {
+                $data [] = $row;
+            }
+            return $data;
+        }
+
+    }
+
+    public function addrecord($data){
+        $this->db->insert('ci4table',$data);
+    }
+    public function deleteRecord($id){
+
+         $this->db->query("DELETE FROM ci4table WHERE id= $id");
+        
+    }
+
+    public function updateRecord($username,$password,$id){
+    
+         $this->db->query("UPDATE  ci4table SET name='$username' , password =$password WHERE id= $id ");
+        
+       
+    }
+    
+
+}
